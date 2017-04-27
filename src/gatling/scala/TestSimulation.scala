9ab88c35ff08086b8b23093b4079e31244b7958c
Markus Thoemmes <markus.thoemmes@de.ibm.com>
2017-04-27 08:11:21 +0200

    Test parameter passing via env

--- src/gatling/scala/TestSimulation.scala
+++ src/gatling/scala/TestSimulation.scala
@@ -3,7 +3,7 @@ import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
 class TestSimulation extends Simulation {
-    val httpProtocol = http.baseURL(s"https://openwhisk.ng.bluemix.net")
+    val httpProtocol = http.baseURL(sys.env("OPENWHISK_HOST"))
 
     val test = scenario("Test Scenario").repeat(1000) {
         exec(http("Info GET").get("/api/v1"))
